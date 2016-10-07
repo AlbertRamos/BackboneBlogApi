3c4afa4fcd0179a5b2a6f9b2b86304169921417a
Seyn <seyn@outlook.com>
2016-10-07 22:17:53 +0200

    Irrelevant 404 styles

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -14,7 +14,7 @@
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                color: #B0BEC5;
+                color: #547c90;
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
@@ -40,7 +40,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Leave me alone.</div>
+                <div class="title">please</div>
             </div>
         </div>
     </body>
